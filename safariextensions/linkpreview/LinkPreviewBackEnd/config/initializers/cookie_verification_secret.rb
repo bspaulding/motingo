03f26233ac0fba434f3278bf0fd8330524beee72
Bradley J. Spaulding <brad.spaulding@gmail.com>
2024-09-21 22:35:27 -0700

    removed safariextensions

--- safariextensions/linkpreview/LinkPreviewBackEnd/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'a1af3346226ed844bc2a4c2f28110e18c768960a96e9303e25e06a74d45be6bbc61ca617a4dfe21f5d9694dcaa9b7138de5a3507afdd634ea8828d27236041fd';
--- safariextensions/linkpreview/LinkPreviewBackEnd/test/unit/helpers/link_previews_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class LinkPreviewsHelperTest < ActionView::TestCase
-end
